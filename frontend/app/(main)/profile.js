72f58f16abe3eba29e2e9a1a4d4a15f458cc06f0
Fatih <16fay61@gmail.com>
2025-07-08 00:33:29 +0200

    Updated some packages, added logout button

--- frontend/app/(main)/profile.js
+++ frontend/app/(main)/profile.js
@@ -1,13 +1,31 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import React from "react";
+import useAuthStore from "../../src/context/useAuthStore";
+import { useRouter } from "expo-router";
 
 const Profile = () => {
+    const logout = useAuthStore((state) => state.logout);
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        await logout();
+        router.replace("/"); // Redirect to login screen
+    };
+
     return (
         <SafeAreaView className="flex-1 bg-background">
             <View className="flex-1 items-center justify-center">
                 <Text className="text-2xl text-text-primary">
                     Profile Screen
                 </Text>
+                <TouchableOpacity
+                    className="mt-8 bg-primary px-6 py-3 rounded-lg"
+                    onPress={handleLogout}
+                >
+                    <Text className="text-white font-semibold text-base">
+                        Logout
+                    </Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
